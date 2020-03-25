061ee8da63d6dde5500824fc8721384c58157892
Anna <filatovaa@yahoo.com>
2020-03-25 14:44:39 -0700

    updated

--- day-1/fibonacci.js
+++ day-1/fibonacci.js
@@ -1,7 +1,9 @@
+//Using recursive function//
+
 var fibonacci = function(n){
     return n < 2 ? n : fibonacci(n-1) + fibonacci(n-2);
 }
 
 for (let i = 0; i<=10; i++){
     console.log(`${(i)} : ${fibonacci(i)}`)
-}
+} 

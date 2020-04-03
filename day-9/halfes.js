c6ebca139433b50b093baae32ea2319c41003705
Anna <filatovaa@yahoo.com>
2020-04-02 21:26:16 -0700

    challenfge solved

--- day-9/halfes.js
+++ day-9/halfes.js
@@ -2,3 +2,20 @@
 
 // Examples
 // halfQuarterEighth(6) ➞ [3, 1.5, 0.75]
+
+function halves(el){
+    const arr = [];
+    arr.push(el/2)
+
+    for(let i=0; i<2; i++){
+        arr.push(arr[i]/2)
+    }
+    return arr
+}
+
+console.log(halves(5))
+//[ 2.5, 1.25, 0.625 ]
+console.log(halves(11))
+//[ 5.5, 2.75, 1.375 ]
+console.log(halves(21))
+//[ 10.5, 5.25, 2.625 ]

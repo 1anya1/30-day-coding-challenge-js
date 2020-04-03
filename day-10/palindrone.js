35c4290935530197a723c704e5b819530dc219f8
Anna <filatovaa@yahoo.com>
2020-04-03 14:41:25 -0700

    solution

--- day-10/palindrone.js
+++ day-10/palindrone.js
@@ -4,3 +4,14 @@
 // isPalindrome(838) ➞ true
 
 // isPalindrome(4433) ➞ false
+
+
+function isPalindrome(n) {
+	let m = Number(n.toString().split('').reverse().join(''))
+	return n===m
+}
+
+console.log(isPalindrome(123))
+//false
+console.log(isPalindrome(12321))
+//true

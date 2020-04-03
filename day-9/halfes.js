977696392b765101066693053df7da6a35c7b872
Anna <filatovaa@yahoo.com>
2020-04-02 21:22:54 -0700

    halfes, quaters and eighth

---
+++ day-9/halfes.js
@@ -0,0 +1,4 @@
+// Create a function that takes a number and return an array of three numbers: half of the number, quarter of the number and an eighth of the number.
+
+// Examples
+// halfQuarterEighth(6) ➞ [3, 1.5, 0.75]

84e58187cbdeda0a017170958e014df58ac39027
Anna <filatovaa@yahoo.com>
2020-04-07 22:40:08 -0700

    logic added to hangman game

--- day-12/hangman/main.js
+++ day-12/hangman/main.js
@@ -1,11 +1,10 @@
-
-
 var words = [
     'dog',
     'cat',
     'flag'
 ];
 
+//Pulling a random word from array 
 var word = words[Math.floor(Math.random()*words.length)];
 console.log(word)
 var answerArray = [];
@@ -13,29 +12,46 @@ for(i=0; i<word.length; i++){
     answerArray[i]='_'
 }
 var remainingLetters = word.length;
-const guesses = [];
+const guessedLetters = [];
+let tries = 0;
 
-while (remainingLetters > 0 ) {
+while (tries < (word.length+6) ) {
     alert(answerArray.join(' '));
+    tries ++;
 
     var guess = prompt('Guess a letter or click Cancel to stop playing')
+    console.log(guess)
+
+    //creating array with al the letters that have been called 
+    guessedLetters.push(guess)
+    console.log(guessedLetters)
+
     if(guess === null){
         break;
     } else if(guess.length !== 1){
         alert("Please enter a single letter")
     } else{
         for(var j=0; j < word.length; j++){
-            if(word[j] === guess){
-                console.log(word[j])
+            if(word[j] === guess && answerArray[j]!== guess){
+                console.log(`iteration of the word: ${word[j]}`)
                 answerArray[j] = guess;
                 remainingLetters --;
-        } else{
-            guesses.push(guess)
-            console.log(guesses)
+                console.log(`Remaining Letters: ${remainingLetters}`)
+        } 
+    }
+    if(tries>= (word.length+6)){
+        alert('Sorry you lost')
+        alert(`The word was ${word}`)
+        break;
+    } else {
+        if(remainingLetters===0){
+            alert('congrats you won')
+            break;
         }
     }
 
+
 }
 }
-alert(answerArray.join(' '))
-alert('Good job')
+
+alert('New Game')

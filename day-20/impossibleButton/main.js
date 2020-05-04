ed5da9fa9b5a843235b6ba2cf8e99b66cd85f649
Anna <filatovaa@yahoo.com>
2020-05-04 00:55:51 -0700

    setting up turns for players

--- day-20/impossibleButton/main.js
+++ day-20/impossibleButton/main.js
@@ -1,8 +1,25 @@
 const btn = document.querySelector('#btn');
 const reset = document.querySelector('#reset');
+const count = document.createElement('h3');
+//dynamically adding nodes to the treee 
+const player1 = document.createElement('h2');
+player1.innerText = 'Player 1'
+player1.appendChild(count)
+
+const player2 = document.createElement('h2');
+player2.innerText = 'Player 2'
+player2.appendChild(count)
+const board = document.querySelector('#players');
+board.appendChild(player1);
+board.appendChild(player2);
+
+
+
+
 
 document.addEventListener('load', ()=>{
     btn.classList.add('start');
+    board.innerText = '0';
     
     
 })

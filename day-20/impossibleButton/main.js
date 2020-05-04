adcc3118d0f4a82fb014f2a1ce8a017c6315289e
Anna <filatovaa@yahoo.com>
2020-05-04 00:42:39 -0700

    added reset button

--- day-20/impossibleButton/main.js
+++ day-20/impossibleButton/main.js
@@ -1,2 +1,27 @@
-const bttn = document.querySelector('button');
-bttn.addEventListener('click',() => console.log('clicked'));
+const btn = document.querySelector('#btn');
+const reset = document.querySelector('#reset');
+
+document.addEventListener('load', ()=>{
+    btn.classList.add('start');
+    
+    
+})
+reset.addEventListener('click', ()=>{
+    btn.classList.remove('win');
+    btn.innerText = 'ClickMe'
+})
+
+
+btn.addEventListener('mouseover',() => {
+    console.log('mousedove')
+    const height = Math.floor(Math.random() * window.innerHeight);
+    const width = Math.floor(Math.random()* window.innerWidth);
+    btn.style.left = `${width}px`;
+    btn.style.top = `${height}px`;
+});
+
+btn.addEventListener('click', () => {
+    btn.innerText = 'You Got Me';
+    btn.classList.add('win')
+
+})

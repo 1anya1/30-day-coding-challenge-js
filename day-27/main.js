6a200e05352d2b9386db3b29274f43c8fc6f86e2
Anna <filatovaa@yahoo.com>
2020-05-17 22:38:18 -0700

    problem completed

--- day-27/main.js
+++ day-27/main.js
@@ -6,3 +6,19 @@
 // censor("Two plus three is five") ➞ "Two plus ***** is five"
 
 // censor("aaaa aaaaa 1234 12345") ➞ "aaaa ***** 1234 *****
+
+function sensor(str) {
+    const arr = str.split(' ');
+    let answer = arr.map(el=>{
+        if(el.length >4){
+            let i='*';
+            return i.repeat(el.length)
+        } else {
+            return el
+        }
+    })
+    return answer.join(' ')
+}
+
+
+console.log(sensor("The code is fourty"))

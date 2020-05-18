5d70728c524efb64e9194bf01ed29f903dd1ec45
Anna <filatovaa@yahoo.com>
2020-05-18 13:35:41 -0700

    day 28

---
+++ day-28/clearTheFog.js
@@ -0,0 +1,8 @@
+// Create a function which returns the word in the string, but with all the fog letters removed. However, if the string is clear from fog, return "It's a clear day!".
+
+// Examples
+// clearFog("sky") ➞ "It's a clear day!"
+
+// clearFog("fogfogfffoooofftreesggfoogfog") ➞ "trees"
+
+// clearFog("fogFogFogffffooobirdsandthebeesGGGfogFog") ➞ "birdsandthebees"
